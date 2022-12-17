7c471d0da853a2dbf53cc7931c02c57101844268
firassamoudi <36210857+firassamoudi@users.noreply.github.com>
2022-12-18 00:16:39 +0100

    api https

--- main.js
+++ main.js
@@ -7652,8 +7652,8 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    // serverUrl: 'https://api.eot.tn'//'http://41.231.122.41:1337'
-    serverUrl: 'http://51.222.106.234:1337' //'http://41.231.122.41:1337'
+    serverUrl: 'https://api.eot.tn' //'http://41.231.122.41:1337'
+    //serverUrl: 'http://51.222.106.234:1337'//'http://41.231.122.41:1337'
     //serverUrl: 'http://localhost:1337'//'http://41.231.122.41:1337'
     // serverUrl: 'http://41.231.122.41:1337'
 };
